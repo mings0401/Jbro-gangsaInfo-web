ef012916d69e175662ed31560f712fc023583f9b
kwangjeong <david@irm.kr>
2017-01-12 23:57:01 +0900

    Complete main.html

---
+++ js/main/notice_board.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kwangjeong on 2017. 1. 12..
+ */
+var notice_board_api = {
+    selectNoticeBoard:function () {
+           
+    },
+    
+    updateNoticeBoard:function () {
+        
+    },
+    
+    deleteNoticeBoard:function () {
+        
+    },
+    
+    insertNoticeBoard:function () {
+        
+    }
+};
