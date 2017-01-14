bc836ae2b8c085ac44be42274dcf15320b24ad4c
kwangjeong <david@irm.kr>
2017-01-14 23:48:26 +0900

    수정.. top 은.. append로 해야할 것 같습니다. 모바일 버전을 위해서

--- js/main/responsible.js
+++ js/main/responsible.js
@@ -2,15 +2,50 @@
  * Created by kwangjeong on 2017. 1. 13..
  */
 window.onresize = function(event) {
+    init();
+};
+
+function init() {
     var parent_height = $(window).height();
     var parent_width = $(window).width();
 
-    if(parent_width < 800) {
+    if(parent_width < 700) {
         $('.div-main-banner').hide();
+
+
     }else {
+        var banner_image_width;
+
         $('.div-main-banner').show();
-        var banner_image_width = parent_width - 400;
-        $('.div-main-banner img').css({'width':banner_image_width});
+
+        if(parent_width - 400 < 790) {
+            banner_image_width = parent_width - 400;
+        }else {
+            banner_image_width = 780;
+        }
+
+
+
+        $('.div-main-banner img').css({
+            'width':banner_image_width
+        });
     }
 
-};
+    if(parent_width < 1100 && parent_width > 800) {
+        $('.main-count-contents-img').show();
+
+        $('.div-main-count-contents').css({
+            'margin-left':'20px'
+        });
+    }else if(parent_width < 800) {
+        $('.main-count-contents-img').hide();
+
+    }else {
+        $('.main-count-contents-img').show();
+
+        $('.div-main-count-contents').css({
+            'margin-left':'120px'
+        });
+    }
+
+}
